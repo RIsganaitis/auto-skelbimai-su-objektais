5cc76101f7d4c0d2257641ddbd05b9ae2b5c957e
RIsganaitis <ramunas.isganaitis@gmail.com>
2022-01-25 10:19:41 +0200

    Logic working. css needs some corrections

--- code.js
+++ code.js
@@ -5,11 +5,9 @@ function addData(e){
     const model = " " + document.getElementById("carModel").value;
     const price = document.getElementById("carPrice").value;
     const picture = document.getElementById("carPicture").value;
-    const id = Date.now();
+    const id = Date.now(); //id kainos span'ui
     const newAdd = new CarAdvert(brand, model, price, picture, id);
     newAdd.addPhotos();
-    // newAdd.alertPrice();
-    return id;
 }
 class CarAdvert {
     constructor(brand, model, price, picture, id){
@@ -27,90 +25,15 @@ class CarAdvert {
             document.querySelector(".autoPictures > div:last-child").append(createPicture);
             document.querySelector(".autoPictures > div:last-child").append(createSpan);
             document.querySelector(".autoPictures > div:last-child").append(createSpanPrice);
+            this.myFunction = function(){       //Alertinimo funkcija
+                alert("Kaina: " + price + "eur")//
+            }                                   //
             createSpan.textContent = this.brand + this.model + " ";
-            createSpanPrice.textContent = this.price + " €";
+            createSpanPrice.textContent = "€" + this.price;
             createPicture.src = this.picture;
-            document.querySelector(".autoPictures > div:last-child > span:last-child").id = this.id;
-            document.querySelector(".autoPictures > div:last-child").className = "block";
-            // document.querySelector(".autoPictures > div:last-child > span:last-child").id = this.id + "id";
+            document.querySelector(".autoPictures > div:last-child > span:last-child").id = this.id; //id kainos span'ui
+            document.querySelector(".autoPictures > div:last-child").className = "block"; //klasė div blokui, kuriame įrašoma info apie vieną skelbimą
+            createDiv.addEventListener("click", this.myFunction); //event listener
         }
-        //
-        this.items = document.getElementsByClassName(".block");
-        this.myFunction = function() {
-            alert("attribute");
-        };
-        Array.from(this.items).forEach(function(element) {
-            element.addEventListener('click', myFunction);
-          });
-        //
-        // this.alertPrice = function(){
-        //     alert(this.price);
-        // }
     }
 }
-
-////
-// const items = document.getElementsByClassName(".block");
-// // this.myFunction = function() {
-// //     alert("attribute");
-// // };
-// Array.from(this.items).forEach(function(element) {
-//     element.addEventListener('click', myFunction);
-//   });
-// ////
-
-
-
-document.querySelector(".autoPictures").addEventListener("click", alertPrices);
-function alertPrices(e){
-    // e.preventDefault();
-    var prices = document.querySelector(".autoPictures > div");
-    var newId = prices.id;
-    // newAdd.myFunction();
-    console.log("document.getElementById(newId).innerText))");
-    // alert(document.getElementById("123").value);
-    // alert(document.getElementById("a123")[0].innerText);
-}
-
-
-
-// document.querySelector(".block").addEventListener("click", alertPrices);
-// function alertPrices(e){
-//     // e.preventDefault();
-//     var prices = document.querySelector(".autoPictures > div > span:last-child");
-//     var newId = prices.id;
-//     var alertPrice = document.getElementById(newId).innerText
-//     console.log(alertPrice);
-//     return alertPrice;
-    // alert(document.getElementById("123").value);
-    // alert(document.getElementById("a123")[0].innerText);
-// }
-
-// const items = document.getElementsByClassName("block");
-// items.forEach((item) => {
-//     item.addEventListener("click", alertPrices);
-//   });
-
-
-//   var items = document.getElementsByClassName(".block");
-//     items.forEach(function(item) {
-//       item.addEventListener("click", () => alert("alertPrices()"));
-//     });
-    
-
-
-//     var myFunction = function() {
-//         var attribute = this.getAttribute("data-myattribute");
-//         alert(attribute);
-//     };
-    
-//     for (var i = 0; i < elements.length; i++) {
-//         elements[i].addEventListener('click', myFunction, false);
-//     }
-//     jQuery does the looping part for you, which you need to do in plain JavaScript.
-    
-//     If you have ES6 support you can replace your last line with:
-    
-//         Array.from(elements).forEach(function(element) {
-//           element.addEventListener('click', myFunction);
-//         });
